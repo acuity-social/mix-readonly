18a44fed21ab4141442ce41eef7597d8166a7f45
Jonathan Brown <jbrown@mix-blockchain.org>
2020-05-02 00:40:19 +0700

    dapp init MixReadonly

---
+++ src/MixReadonly.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.15;
+
+import "ds-test/test.sol";
+
+import "./MixReadonly.sol";
+
+contract MixReadonlyTest is DSTest {
+    MixReadonly readonly;
+
+    function setUp() public {
+        readonly = new MixReadonly();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
